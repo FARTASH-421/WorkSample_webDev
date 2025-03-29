ad52530c06afefd5a733404b4c6414121953ed49
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-03-29 04:44:28 +0330

    change somthings

---
+++ toDoList/index.js
@@ -0,0 +1,12 @@
+const inputAdd = document.getElementById("inputAdd");
+const titleBox = document.getElementById("titleBox");
+const addBut = document.getElementsByClassName("myAdd")
+
+
+function addItem(){
+    
+    // titleBox.childElementCount
+    inputAdd.style.display = inputAdd.style.display === "flex"? "none": "flex";
+    // addBut.style.position = "fixed";
+    
+}
