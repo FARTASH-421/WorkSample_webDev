75b39b2b9e11cc1a693d5cacdce070eae49e2674
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-03-29 05:01:05 +0330

    practice consturactor object

--- Practices/index.js
+++ Practices/index.js
@@ -1,1 +1,28 @@
 
+function Human(fN, lN, age){
+    this.fName = fN;
+    this.lName = lN;
+    this.age = age;
+
+    this.getAge = function(){return this.age};
+    this.getLname = function(){return this.lName};
+    this.getFName = function () {return this.fName};
+
+    this.display = function(){
+        console.log(`my frist name is ${this.getFName()}\nmy last name ${this.getLname()}\nI am ${this.getAge()} years old!`)
+    }
+}
+
+
+const human1 = new Human("A.Qadir", "Fartash", 23);
+human1.display();
+console.log(human1.getAge())
+console.log(human1.fName);
+console.log(human1.getLname());
+console.log("\n");
+
+const h2 = new Human ("A.Samad", "Mahdi Yar", 24);
+h2.display();
+console.log(h2.getAge())
+console.log(h2.fName);
+console.log(h2.getLname());
