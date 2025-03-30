6e2f12dcc9ee337239ed5080cca07960d79bd647
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-03-30 06:51:56 +0330

    style and html file completed

---
+++ Pomodoro Timer/PomodoroTimer.js
@@ -0,0 +1,18 @@
+const start =document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+const display = document.getElementById('display');
+
+const bt = document.getElementById("Text");
+
+
+function editFun(){
+    
+    bt.style.visibility = "visible";
+    // reset.style.backgroundColor = "red";
+}
+
+
+function closeFun(){
+    bt.style.visibility = "hidden";
+}
