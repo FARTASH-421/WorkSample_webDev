de9df4bab0ad4211c86793099a8f3c3a443127ab
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-04-05 21:26:21 +0330

    somthings changes

--- Pomodoro Timer/PomodoroTimer.js
+++ Pomodoro Timer/PomodoroTimer.js
@@ -2,17 +2,24 @@ const start =document.getElementById('start');
 const stop = document.getElementById('stop');
 const reset = document.getElementById('reset');
 const display = document.getElementById('display');
+const boxInpu = document.getElementById('boxInpu');
+const textBt = document.getElementById('textBt');
 
 const bt = document.getElementById("Text");
 
 
 function editFun(){
     
-    bt.style.visibility = "visible";
+    boxInpu.style.visibility = "visible";
     // reset.style.backgroundColor = "red";
 }
 
 
 function closeFun(){
-    bt.style.visibility = "hidden";
+    boxInpu.style.visibility = "hidden";
+}
+
+function submetFun(){
+
+    boxInpu.style.visibility = "hidden";
 }
